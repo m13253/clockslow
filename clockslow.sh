222e12a4c10addde74acd7ffa4cf6e6b525dbba1
Star Brilliant <m13253@hotmail.com>
2014-05-06 01:49:43 +0800

    Add GPL header

--- clockslow.sh
+++ clockslow.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Clockslow -- A tool to trick app to let it think time goes slower or faster
+# Copyright (C) 2014  StarBrilliant <m13253@hotmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if [ "$#" -lt 2 ]
 then
     echo 'Clockslow: Trick app to let it think time goes slower or faster, inspired by'
