2669b1b5a5e2b6c9b2dc67d79eb08c2b13a88f5c
Star Brilliant <m13253@hotmail.com>
2014-05-18 16:18:44 +0800

    Switch to LGPL license

--- clockslow.sh
+++ clockslow.sh
@@ -3,18 +3,19 @@
 # Clockslow -- A tool to trick app to let it think time goes slower or faster
 # Copyright (C) 2014  StarBrilliant <m13253@hotmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
 
 if [ "$#" -lt 1 ]
 then
